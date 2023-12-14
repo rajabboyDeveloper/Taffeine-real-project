8c66433865c75ea8e7581518e0c9b6d2d8d05e00
rajabboy <coder@rajabboys-MacBook-Pro.local>
2023-12-14 16:34:10 +0500

    ya dobavil sve papki

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
